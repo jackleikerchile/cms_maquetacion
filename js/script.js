a205f91c329b7c0fddcf43d3ae4f5b54e15a83c7
jackleiker <jackleikerchile@gmail.com>
2018-10-27 01:24:15 -0300

    finish articles and gallery of videos
    code of articles gallery and begin of google maps and contacts

--- js/script.js
+++ js/script.js
@@ -1,1 +1,114 @@
-script.js
+/*=============================================
+SLIDE            
+=============================================*/
+var numeroSlide = 1;
+var formatearLoop = false;
+var cantidadImg = $("#slide ul li").length;
+
+$("#slide ul").css({"width": (cantidadImg*100) + "%"})
+$("#slide ul li").css({"width": (100/cantidadImg) + "%"})
+
+/* INDICADORES */
+
+$("#indicadores li").click(function(){
+
+	 var roleSlide = $(this).attr("role-slide");
+			
+	 $("#slide ul li").css({"display":"none"});
+			
+	 $("#slide ul li:nth-child("+roleSlide+")").fadeIn();
+			
+	 $("#indicadores li").css({"opacity":".5"});
+			
+	 $("#indicadores li:nth-child("+roleSlide+")").css({"opacity":"1"});
+
+	 formatearLoop = true;
+
+	numeroSlide = roleSlide;
+
+})
+
+/* FLECHA AVANZAR */
+
+function avanzar(){
+
+	if(numeroSlide >= cantidadImg){numeroSlide = 1;}
+
+	else{numeroSlide++}
+
+	$("#slide ul li").css({"display":"none"});
+			
+	$("#slide ul li:nth-child("+numeroSlide+")").fadeIn();
+			
+	$("#indicadores li").css({"opacity":".5"});
+			
+	$("#indicadores li:nth-child("+numeroSlide+")").css({"opacity":"1"});
+}
+
+
+$("#slideDer").click(function(){
+
+	avanzar();
+
+	formatearLoop = true;
+
+})
+
+/* FLECHA RETROCEDER */
+
+$("#slideIzq").click(function(){
+
+	if(numeroSlide <= 1){numeroSlide = cantidadImg;}
+
+	else{numeroSlide--}
+
+
+	$("#slide ul li").css({"display":"none"});
+			
+	$("#slide ul li:nth-child("+numeroSlide+")").fadeIn();
+			
+	$("#indicadores li").css({"opacity":".5"});
+			
+	$("#indicadores li:nth-child("+numeroSlide+")").css({"opacity":"1"});
+
+	formatearLoop = true;
+
+})
+
+/* LOOP */
+
+setInterval(function(){
+
+	if(formatearLoop == true){
+
+		formatearLoop = false;
+	}
+
+	else{
+
+	avanzar();
+
+	}
+
+},5000);
+
+/*=====  Fin de SLIDE  ======*/
+
+
+
+
+/*=============================================
+Google Maps          
+=============================================*/
+
+function myMap() {
+var mapProp= {
+    center:new google.maps.LatLng(51.508742,-0.120850),
+    zoom:5,
+};
+var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
+
+
+
+/*=====  Fin de Google Maps  ======*/
