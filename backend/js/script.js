b183c20c8777a95febbd92ad54019482104b9f51
jackleiker <jackleikerchile@gmail.com>
2018-12-19 01:45:01 -0300

    finished the part desktop of begin for the aplication
    position a neat list of administrator putting javascript

---
+++ backend/js/script.js
@@ -0,0 +1,13 @@
+/*=============================================
+=            Buttoms administrator            =
+=============================================*/
+$("#member i").click(function(){
+	$("#admin").slideToggle("fast");
+	$("#member i").toggleClass("angle-down");
+	$("#member i").toggleClass("angle-up");
+})
+
+
+
+/*=====  End of Buttoms administrator  ======*/
+
